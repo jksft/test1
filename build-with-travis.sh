f955fb5fc8354ca5828452230958ca1b27a3f63f
jksft <13396937258@163.com>
2019-07-26 20:23:04 +0800

    update

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,5 +1,5 @@
 mkdir /build && cd /build
-repo init -q -u git://github.com/CyanogenMod/android.git -b cm-12.1 && repo sync -j32
+repo init -q --depth=1 -u git://github.com/CyanogenMod/android.git -b cm-12.1 && repo sync -j32
 rm -rf .repo && find . -type d -name ".git" -execdir rm -rf {} \;
 mkdir -p device/oppo/A37 && cp -r /sources/* device/oppo/A37
 source build/envsetup.sh && lunch cm_A37-user && make -j8 bootimage
